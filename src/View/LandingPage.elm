29b4570da46c4f3ffaf7db04f15a9063c6e1bb2a
Brad <bradpf@outlook.com>
2018-03-14 00:57:58 -0600

    Working on landing page and video element
    - Adding Icon view for material icons

--- src/View/LandingPage.elm
+++ src/View/LandingPage.elm
@@ -1,13 +1,27 @@
 module View.LandingPage exposing (landingView)
 
 
-import Html exposing (Html, div, text)
-import Model exposing (Model)
-import Messages exposing (Msg)
+import Html exposing (Html, div, text, h1, input, button)
+import Html.Attributes exposing (class, id, type_)
+import Html.Events exposing (onClick)
+import Model exposing (Model, Route(..))
+import Messages exposing (Msg(..))
 
 
 landingView : Model -> Html Msg
 landingView model =
-    div []
-        [ text "Landing Page!!"
+    div [ id "landing" ]
+        [ div
+            [ class "action host"
+            , onClick (Navigate VideoRoute)
+            ]
+            [ h1 [ class "title" ] [ text "host" ]
+            ]
+        , div
+            [ class "action watch"
+            , onClick (Navigate VideoRoute)
+            ]
+            [ h1 [ class "title" ] [ text "watch" ]
+            , input [ class "hidden-field uuid-field" ] []
+            ]
         ]
--- src/View/VideoPage.elm
+++ src/View/VideoPage.elm
@@ -1,13 +1,19 @@
 module View.VideoPage exposing (videoView)
 
 
-import Html exposing (Html, div, text)
+import Html exposing (Html, text, div, video, i)
+import Html.Attributes exposing (class, id)
 import Model exposing (Model)
 import Messages exposing (Msg)
+import View.Icon exposing (lightActiveIcon)
 
 
 videoView : Model -> Html Msg
 videoView model =
-    div []
-        [ text "Video Page!!"
+    div [ id "video" ]
+        [ div [ class "fake-video" ]
+            [ div [ class "controls" ]
+                [ lightActiveIcon "play_arrow"
+                ]
+            ]
         ]
